fee44039dbf738a86180a3bab9d467252467862f
April De Zen <aprildezen@Aprils-MacBook.local>
2018-09-30 20:42:36 -0400

    adding links

--- capture_1_ADZ.js
+++ capture_1_ADZ.js
@@ -17,8 +17,17 @@ function setup() {
     createCanvas(w, h);
     capture.hide();
 }
-
-
+//ADD EFFECT HERE
 function draw() {
     image(capture, 0, 0);
+        noStroke();
+var c = color(0, 126, 255, 102);
+fill(c);
+rect(0, 0, 300, 1000);
+    var c = color(0, 0, 255, 102);
+fill(c);
+rect(300, 0, 350, 3000);
+var value = alpha(c); // Sets 'value' to 102
+fill(value);
+ellipse(300, 150, 100, 100);
 }
