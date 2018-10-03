5cb498213ff9dc5f4a3a6e1b5b520c8161d8cf53
April De Zen <aprildezen@aprils-macbook.acs.ocad.ca>
2018-10-03 19:58:49 -0400

    Added a 3D box

--- sketch.js
+++ sketch.js
@@ -1,7 +1,25 @@
+let wolf;
+let angle = 0;
+
+
+function preload(){
+	//wolf = loadModel('Wolf.obj');
+}
+
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(400, 400, WEBGL);
+}
 
 function draw() {
-    background(220);
+    background(0);
+    ambientLight(100);
+    directionalLight(255,255,255,0,0,1)
+    rotateX(angle);
+    rotateY(angle * 1.3);
+    rotateZ(angle * 0.7);
+    box(100);
+    //Model(wolf);
+    angle += 0.03;
+
 
 }
