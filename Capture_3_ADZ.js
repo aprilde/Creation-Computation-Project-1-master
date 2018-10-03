9dc0ce83fb30555585a2c24afc10b60e17c56583
April De Zen <aprildezen@aprils-macbook.acs.ocad.ca>
2018-10-03 19:03:14 -0400

    working captures

---
+++ Capture_3_ADZ.js
@@ -0,0 +1,91 @@
+var capture;
+var system;
+var w = 1000;
+var h = 480;
+
+function setup() {
+    capture = createCapture({
+        audio: false,
+        video: {
+            width: w*2,
+            height: h*2
+        }
+
+    }, function() {
+        console.log('capture ready.')
+    });
+    capture.elt.setAttribute('playsinline', '');
+    capture.size(w, h);
+    createCanvas(w, h);
+    capture.hide();
+    system = new ParticleSystem(createVector(width/2, 50));
+
+}
+
+//ADD EFFECT HERE
+function draw() {
+    image(capture, 0, 0);
+        noStroke();
+//COLOUR OVERLAY
+//var c = color(0, 126, 255, 102);
+//fill(c);
+  system.addParticle();
+  system.run();
+}
+
+// A simple Particle class
+var Particle = function(position) {
+  this.acceleration = createVector(0, 0.05);
+  this.velocity = createVector(random(-1, 1), random(-1, 0));
+  this.position = position.copy();
+  this.lifespan = 255;
+};
+
+Particle.prototype.run = function() {
+  this.update();
+  this.display();
+};
+
+// Method to update position
+Particle.prototype.update = function(){
+  this.velocity.add(this.acceleration);
+  this.position.add(this.velocity);
+  this.lifespan -= 2;
+};
+
+// Method to display
+Particle.prototype.display = function() {
+  stroke(200, this.lifespan);
+  strokeWeight(2);
+  fill(127, this.lifespan);
+  ellipse(this.position.x, this.position.y, 12, 12);
+};
+
+// Is the particle still useful?
+Particle.prototype.isDead = function(){
+  return this.lifespan < 0;
+};
+
+var ParticleSystem = function(position) {
+  this.origin = position.copy();
+  this.particles = [];
+};
+
+ParticleSystem.prototype.addParticle = function() {
+  this.particles.push(new Particle(this.origin));
+};
+
+ParticleSystem.prototype.run = function() {
+  for (var i = this.particles.length-1; i >= 0; i--) {
+    var p = this.particles[i];
+    p.run();
+    if (p.isDead()) {
+      this.particles.splice(i, 1);
+    }
+  }
+};
+
+
+
+
+
