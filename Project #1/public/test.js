2ce324f53849c186f29ae57be11aa424be3040a7
Joshua McKenna <josh@Joshuas-MacBook-Pro.local>
2018-10-02 23:41:21 -0400

    11:41PM Update

---
+++ Project #1/public/test.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(1000, 1000, WEBGL);
+  video = createCapture(VIDEO);
+  video.hide();
+}
+
+function draw() {
+  background(255, 255, 255, 0);
+  rotateX(frameCount * 0.001);
+  rotateY(frameCount * 0.001);
+  //pass image as texture
+  texture(video);
+  box(200, 200, 200);
+}
