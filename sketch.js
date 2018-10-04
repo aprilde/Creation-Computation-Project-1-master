acb6572d03eeb26a493aacba4433385a53b43a9e
April De Zen <aprildezen@aprils-macbook.acs.ocad.ca>
2018-10-03 21:28:55 -0400

    trying to add my own 3D model

--- sketch.js
+++ sketch.js
@@ -1,9 +1,9 @@
-let wolf;
+var wolf;
 let angle = 0;
 
 
 function preload(){
-	//wolf = loadModel('Wolf.obj');
+	wolf = loadModel('model/Wolf.obj');
 }
 
 function setup() {
@@ -11,14 +11,16 @@ function setup() {
 }
 
 function draw() {
-    background(0);
+    background(175);
     ambientLight(100);
     directionalLight(255,255,255,0,0,1)
-    rotateX(angle);
-    rotateY(angle * 1.3);
-    rotateZ(angle * 0.7);
-    box(100);
-    //Model(wolf);
+    rotateX(frameCount * 0.01);
+  	rotateY(frameCount * 0.01);
+    //rotateX(angle);
+    //rotateY(angle * 1.3);
+    //rotateZ(angle * 0.7);
+    //box(100);
+    model(wolf);
     angle += 0.03;
 
 
