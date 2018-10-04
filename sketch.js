7b9a16379e64684339db9d17dc85c631b22d878d
April De Zen <aprildezen@aprils-macbook.acs.ocad.ca>
2018-10-03 22:34:33 -0400

    3D Cam on wolf

--- sketch.js
+++ sketch.js
@@ -1,33 +1,30 @@
 var wolf;
 let angle = 0;
+let cam;
 
 function preload(){
 	wolf = loadModel('model/Wolf.obj');
- 	//normalize();
 }
 
-/*
-hhhhhh
-hhhhh
-hhhhhhg
-
-*/
 
 function setup() {
     createCanvas(400, 400, WEBGL);
+    cam = createCapture(VIDEO);
+    cam.hide();
 }
 
 function draw() {
     background(175);
-    ambientLight(100);
-    directionalLight(255,255,255,0,0,1)
+    ambientLight(255);
+    //directionalLight(255,255,255,0,0,1)
     rotateX(frameCount * 0.01);
   	rotateY(frameCount * 0.01);
     rotateX(angle);
     rotateY(angle * 1.3);
     //rotateZ(angle * 0.7);
     //box(100);
-	model(wolf);
+    model(wolf);
+	texture(cam);
     // angle += 0.03;
 
 
